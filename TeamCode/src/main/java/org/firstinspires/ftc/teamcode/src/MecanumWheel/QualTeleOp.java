ad5eb00eac32e1786691c172d5e7c4f4980f834a
Chain Reaction <chainreaction12586@gmail.com>
2024-01-04 19:07:51 -0600

    new configurations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/MecanumWheel/QualTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/MecanumWheel/QualTeleOp.java
@@ -1,20 +1,20 @@
 package org.firstinspires.ftc.teamcode.src.MecanumWheel;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-@Disabled
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "✝TELEOP✝", group = "COMPETITION")
-public class TeleOp extends GenericOpmoodeTemplate {
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "✝QUAL_TELEOP✝", group = "COMPETITION")
+public class QualTeleOp extends QUALGenericOpmoodeTemplate{
+
     public static String getStackTraceAsString(Throwable e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
         return sw.toString();
     }
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -25,6 +25,10 @@ public class TeleOp extends GenericOpmoodeTemplate {
             while (!isStarted() && !isStopRequested()) {
                 Intake1.setPosition(0.3);
                 Intake2.setPosition(0.3);
+                plane_rotate.setPosition(0);
+                Launcher.setPosition(0.34);
+                Outtake_left.setPosition(0.575);
+                Outtake_right.setPosition(0.445);
             }
 
             while (opModeIsActive()) {
@@ -36,6 +40,8 @@ public class TeleOp extends GenericOpmoodeTemplate {
                 gamepadControlDriveTrain(Speed);
 
                 IntakeControl();
+
+                OuttakeControl();
 
                 LauncherControl();
 
@@ -75,26 +81,52 @@ public class TeleOp extends GenericOpmoodeTemplate {
     }
 
     void LauncherControl() {
-        if (gamepad2.y) {
-            Launcher.setPower(1);
-        } else if (gamepad2.a) {
-            Launcher.setPower(-1);
+        if (gamepad2.a) {
+            plane_rotate.setPosition(0);
+        } else if (gamepad2.y) {
+            plane_rotate.setPosition(0.1);
+        }
+        if (gamepad2.b) {
+            Launcher.setPosition(0.34);
+        } else if (gamepad2.start) {
+            Launcher.setPosition(0.2);
         }
     }
 
     void IntakeControl() {
         if (gamepad2.dpad_down) {
-            Intake1.setPosition(0.565);
-            Intake2.setPosition(0.565);
+            Intake1.setPosition(0);
+            Intake2.setPosition(0);
         }
         if (gamepad2.dpad_up) {
-            Intake1.setPosition(0.3);
-            Intake2.setPosition(0.3);
+            Intake1.setPosition(1);
+            Intake2.setPosition(1);
+        }
+    }
+
+    void OuttakeControl() {
+        if (gamepad2.left_bumper) {
+            Outtake_left.setPosition(0.5);
+            Outtake_right.setPosition(0.52);
+        }
+        if (gamepad2.right_bumper) {
+            Outtake_left.setPosition(0.575);
+            Outtake_right.setPosition(0.445);
         }
     }
 
     void TelemetryUpdate(double Speed) {
         telemetry.addData("RUN", getRuntime());
+
+        telemetry.addData("inr", Intake1.getPosition());
+        telemetry.addData("inl", Intake2.getPosition());
+        telemetry.addData("outl", Outtake_left.getPosition());
+        telemetry.addData("outr", Outtake_right.getPosition());
+        telemetry.addData("lunch", Launcher.getPosition());
+        telemetry.addData("rotor", plane_rotate.getPosition());
+        telemetry.addData("exl", extend_left.getPosition());
+        telemetry.addData("exr", extend_right.getPosition());
+
         telemetry.addData("FR_Power", front_right.getPower());
         telemetry.addData("FLPower", front_left.getPower());
         telemetry.addData("BR_Power", back_right.getPower());
@@ -108,4 +140,3 @@ public class TeleOp extends GenericOpmoodeTemplate {
         telemetry.update();
     }
 }
-
